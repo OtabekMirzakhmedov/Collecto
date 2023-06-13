e729dd080e30ed82bdb04e5619ff047f3214bef3
User <User@DESKTOP-C51DDIB>
2023-06-13 13:17:16 +0500

    commiting and deploying

--- Collecto.FE/src/App.js
+++ Collecto.FE/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <h1  className='text-danger mx-auto'>Collecto</h1>
+        <div className='position-absolute top-50 start-50 translate-middle'>
+          <h2>Hello World
+          </h2>
+        </div>
     </div>
   );
 }
