c3ebd93d53cf29b16c2de4f8213db8e893efe2ac
User <User@DESKTOP-C51DDIB>
2023-06-15 15:34:17 +0500

    serilog installed and tested

--- Collecto.BE/Controllers/WeatherForecastController.cs
+++ Collecto.BE/Controllers/WeatherForecastController.cs
@@ -21,11 +21,13 @@ namespace Collecto.BE.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Weather Forecast executing...");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+
             })
             .ToArray();
         }
