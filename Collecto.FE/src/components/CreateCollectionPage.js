5f0710ebb41487fa12653a03d7b02f9e11e7ba43
User <User@DESKTOP-C51DDIB>
2023-07-05 16:28:58 +0500

    feat: form validation for create collection

--- Collecto.FE/src/components/CreateCollectionPage.js
+++ Collecto.FE/src/components/CreateCollectionPage.js
@@ -1,88 +1,181 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useForm , Controller} from "react-hook-form";
+import CreatableSelect from "react-select/creatable";
+import makeAnimated from "react-select/animated";
 import "./components.css";
 
+const animatedComponents = makeAnimated();
+
 const CreateCollectionPage = () => {
-  const [customField, setCustomField] = useState([]);
+  const [customFields, setCustomFields] = useState([]);
+  const [selectedTopic, setSelectedTopic] = useState(null);
+  const [inputTopic, setInputTopic] = useState("");
+  const [options, setOptions] = useState([]);
+
+  useEffect(() => {
+    setOptions([
+      { value: "topic1", label: "Topic 1" },
+      { value: "topic2", label: "Topic 2" },
+      { value: "topic3", label: "Topic 3" },
+    ]);
+  }, []);
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors},
+    control,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  const handleTopicChange = (selectedOption) => {
+    setSelectedTopic(selectedOption);
+
+  };
+  const handleTopicInputChange = (inputValue) => {
+    setInputTopic(inputValue);
+  };
 
   const handleClick = () => {
-    setCustomField([...customField, { fieldName: "", fieldType: "" }]);
+    setCustomFields([...customFields, { fieldName: "", fieldType: "" }]);
   };
-  const handleChange = (e, i) => {
-  };
-  const onSubmit = (e) => {
-    e.preventDefault();
+
+  const handleRemoveProperty = (index) => {
+    const updatedFields = [...customFields];
+    updatedFields.splice(index, 1);
+    setCustomFields(updatedFields);
   };
   return (
     <div className="container">
-      <div className="fs-2 m-auto">Create Collection</div>
-      <form className="m-auto border border-0  col-6" onSubmit={onSubmit}>
+      <div className="align-content-center pt-3">
+        <a href="/">
+          <i className="bi bi-flower1 fs-1 my-1 text-danger"></i>
+        </a>
+        <a className="navbar-brand" href="/">
+          <p className="fs-2 d-inline text-danger mx-2">collecto </p>
+        </a>
+      </div>
+
+      <form
+        className="m-auto border border-0 col-lg-6 col-xl-6 col-12 p-1"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <div className="fs-4 text-center">Collection creation form</div>
         <div className="row border-top">
-          <div className="col-3 text-secondary border-end fs-6 fw-medium p-1">
+          <div className="col-4 text-secondary border-end fs-6 fw-medium p-1">
             Title
           </div>
-          <div className="col-9 p-0">
+          <div className="col-8 p-0">
             <input
               type="text"
-              className="border-0 col-8 p-1  collection-input"
+              className="border-0 w-100 p-1  collection-input"
               placeholder="Empty"
+            
+            {...register("title", { required: true })}
             />
+            {errors.title && (
+            <span className="text-danger">Title is required</span> // Display error message if validation fails
+          )}
           </div>
+          
         </div>
         <div className="row border-top">
-          <div className="col-3 text-secondary border-end fs-6 fw-medium p-1">
+          <div className="col-4 text-secondary border-end fs-6 fw-medium p-1">
             Topic
           </div>
-          <div className="col-9 p-0">
-            <input
-              type="text"
-              className="border-0 col-8 p-1  collection-input"
-              placeholder="Empty"
-            />
+          <div className="col-8 p-0">
+          <Controller
+  control={control}
+  name="topic"
+  defaultValue=""
+  rules={{ required: true }}
+  render={({ field }) => (
+    <CreatableSelect
+      className="border-0 w-100 p-1 collection-input"
+      isClearable
+      isSearchable
+      value={field.value}
+      onChange={(selectedOption) => {
+        field.onChange(selectedOption);
+        
+      }}
+  
+      placeholder="Empty"
+      components={animatedComponents}
+      options={options}
+    />
+  )}
+/>
+
+      {errors.topic && (
+        <span className="text-danger">Topic is required</span>
+      )}
+        
           </div>
+          
         </div>
         <div className="row border-top">
-          <div className="col-3 text-secondary border-end fs-6 fw-medium p-1">
+          <div className="col-4 text-secondary border-end fs-6 fw-medium p-1">
             Description
           </div>
-          <div className="col-9 p-0">
+          <div className="col-8 p-0">
             <textarea
-              className="border-0 col-12 p-1 collection-input"
+              className="border-0 w-100 p-1 collection-input"
               placeholder="Empty"
+              {...register("description", { required: true })}
             />
+            {errors.description && (
+              <span className="text-danger">Description is required</span> // Display error message if validation fails
+            )}
           </div>
-          </div>
-
-          {customField.map((val, i) => (
-            <div className="row border-top">
-              <div className="col-3 text-secondary border-end fs-6 fw-medium p-0">
-                <input
-                 
-                  type="text"
-                  className=" col-12 border-0 p-1   collection-input"
-                  placeholder="Property Name"
-                  onChange={(e) => handleChange(e, i)}
-                />
-              </div>
-              <div className="col-9 p-0">
-                <input
-                 
-                  type="text"
-                  className="border-0 col-8 p-1  collection-input"
-                  placeholder="Empty"
-                  onChange={(e) => handleChange(e, i)}
-                />
-              </div>
+        </div>
+        {customFields.map((field, i) => (
+          <div className="row border-top" key={i}>
+            <div className="col-4 text-secondary border-end fs-6 fw-medium p-0">
+              <input
+                type="text"
+                className="w-100 border-0 p-1 collection-input"
+                placeholder="Property Name"
+              />
             </div>
-          ))}
-          <div className="col-3 text-secondary fs-6 fw-medium p-1">
-            <button className="btn btn-outline-none" onClick={handleClick}>
-              <i className="bi bi-plus-circle"></i> Add Property
-            </button>
+            <div className="col-8 p-0 d-flex justify-content-between">
+              <select
+                className="border-0 col-8 p-1 collection-input"
+                aria-label="Default select example"
+              >
+                <option defaultValue>Select property type</option>
+                <option value="Number">Number</option>
+                <option value="Date">Date</option>
+                <option value="SingleLineText">Single line Text</option>
+                <option value="MultiLineText">Multi line Text</option>
+                <option value="Checkbox">Checkbox</option>
+              </select>
+              <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={() => handleRemoveProperty(i)}
+              ></button>
+            </div>
           </div>
+        ))}
 
+        <div className="w-100 text-secondary fs-6 fw-medium p-1 border-top">
+          <button
+            className="btn btn-outline-none"
+            type="button"
+            onClick={handleClick}
+          >
+            <i className="bi bi-plus-circle"></i> Add Property
+          </button>
+        </div>
 
-          <button type="submit" className="btn btn-primary p-1 m-0">Create</button>
-        
+        <button type="submit" className="btn btn-primary p-1 m-0">
+          Create
+        </button>
       </form>
     </div>
   );
